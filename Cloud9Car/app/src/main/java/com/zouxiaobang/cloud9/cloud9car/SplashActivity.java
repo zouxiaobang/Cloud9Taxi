a35fc1fddf9e811b9cdbb86f4bcf275ed2ebc7c3
zouxiaobang <zouxiaobang@qq.com>
2017-10-14 13:16:50 +0800

    添加项目Cloud9并实现其启动页

---
+++ Cloud9Car/app/src/main/java/com/zouxiaobang/cloud9/cloud9car/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.zouxiaobang.cloud9.cloud9car;
+
+import android.app.Activity;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            AnimatedVectorDrawable anim1 = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.anim);
+            ImageView logo = (ImageView) findViewById(R.id.logo);
+            logo.setImageDrawable(anim1);
+            anim1.start();
+        }
+    }
+}
